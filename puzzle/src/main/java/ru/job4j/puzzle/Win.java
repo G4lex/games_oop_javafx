794fa130efec7ca351cb0651d841eb22e52034cf
G4lex <ganalex01@gmail.com>
2022-05-24 00:35:44 +0300

    classWin ver 1

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,35 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (vert(board, i) || horizont(board, i))) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean horizont(int[][] board, int row) {
         boolean rsl = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean vert(int[][] board, int column) {
+        boolean rsl = true;
+        for (int i = 0; i < board[column].length; i++) {
+            if (board[i][column] != 1) {
+                rsl = false;
+                break;
+            }
+        }
         return rsl;
     }
 }
